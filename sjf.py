b0302e64e915ced5248f979607f5e63344b1dbbb
Bartosz Złotnik <bartekzlotnik@gmail.com>
2024-01-05 17:30:11 +0100

    FIX(sjf algorithm and visualization)

--- sjf.py
+++ sjf.py
@@ -36,7 +36,7 @@ def collecting_data_from_file(path: str):
     rr_time_quantum = int(data[4][0])
 
 
-collecting_data_from_file("data2.txt")
+collecting_data_from_file("data.txt")
 
 
 class SJFVisualizer:
@@ -51,6 +51,7 @@ class SJFVisualizer:
         self.current_time_unit = 0
         self.current_process_index = 0
         self.process_time_counter = []
+        self.last_process_id = 0
 
         self.whole_time = 0
 
@@ -69,24 +70,17 @@ class SJFVisualizer:
         self.ax.xaxis.set_major_locator(MultipleLocator((self.whole_time // 25) + 1))
         self.ax.yaxis.set_major_locator(MultipleLocator(1))
         self.canvas.draw()
-
+        self.initialize_counters()
         self.update_chart()
 
 
     def update_chart(self):
-
+        
         if self.current_time_unit <= self.whole_time:
             
             minimum_burst_time_process_id = 1
             minumim_burst_time = 0
-            available_processes = []
-
-            for i in range(len(self.sjf_data)):
-
-                if self.sjf_data[i]['arrival_time'] <= self.current_time_unit and self.sjf_data[i]['burst_time'] > 0:
-
-                    if i not in available_processes:
-                        available_processes.append(i)
+            available_processes = self.finding_avaliable_processes()
 
             for j, index in enumerate(available_processes):
                 
@@ -100,41 +94,65 @@ class SJFVisualizer:
                     minumim_burst_time = self.sjf_data[index]['burst_time']
 
             
-
-
             self.sjf_data[minimum_burst_time_process_id - 1]['burst_time'] = self.sjf_data[minimum_burst_time_process_id - 1]['burst_time'] - 1
-
-            
 
             self.ax.broken_barh([(self.current_time_unit, 1)], (minimum_burst_time_process_id - 1, 1), 
                                 facecolors=(0.5, 0.5, 0.5), edgecolor='yellow', linewidth=1)
             
-            counter = self.ax.text(0, minimum_burst_time_process_id - 0.5, f"P{minimum_burst_time_process_id}: {self.process_time_counter[minimum_burst_time_process_id - 1]} ", 
-                ha='center', va='center', color="blue", fontweight='bold', fontsize=10)
-            
             self.process_time_counter[minimum_burst_time_process_id - 1] = self.process_time_counter[minimum_burst_time_process_id - 1] - 1
 
+            self.update_counters(minimum_burst_time_process_id)
+            self.draw_x_line(minimum_burst_time_process_id)
             self.canvas.draw()
             
-            
             self.current_time_unit += 1
+            self.last_process_id = minimum_burst_time_process_id
 
+            self.master.after(200, self.update_chart)
             
-            # # # print(available_processes)
-            # # print(self.sjf_data)
-                    
-
-
-
-
-
-
-            self.master.after(500, self.update_chart)
-            # counter.remove() TODO
-
         else:
             pass
 
+
+    def initialize_counters(self):
+
+        self.counter_texts = {}
+
+        for process in self.sjf_data:
+
+            process_id = process['id']
+            self.counter_texts[process_id] = self.ax.text(0, process_id - 0.5, 
+                                                        f"P{process_id}: {self.process_time_counter[process_id - 1]} ", 
+                                                        ha='center', va='center', color="blue", fontweight='bold', fontsize=10)
+
+
+    def update_counters(self, process_id):
+
+        self.counter_texts[process_id].remove()
+        self.counter_texts[process_id] = self.ax.text(0, process_id - 0.5, 
+                                                        f"P{process_id}: {self.process_time_counter[process_id - 1]} ", 
+                                                        ha='center', va='center', color="blue", fontweight='bold', fontsize=10)
+
+
+    def draw_x_line(self, process_id):
+
+        if self.last_process_id != process_id and self.last_process_id != 0:
+
+            self.ax.axvline(x=self.current_time_unit, color='purple', linestyle='solid')
+
+
+    def finding_avaliable_processes(self):
+
+        available_processes = []
+
+        for i in range(len(self.sjf_data)):
+
+            if self.sjf_data[i]['arrival_time'] <= self.current_time_unit and self.sjf_data[i]['burst_time'] > 0:
+
+                if i not in available_processes:
+                    available_processes.append(i)
+
+        return available_processes
 
 
 root = tk.Tk()
